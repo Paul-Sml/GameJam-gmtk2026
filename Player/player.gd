2cd24884f2aefde684e8a30dc3309f82f6728b39
Paul-Sml <paul.semmel.pro@gmail.com>
2026-07-22 23:20:15 +0200

    Custom timer for levels + window size

--- Player/player.gd
+++ Player/player.gd
@@ -2,6 +2,13 @@ extends CharacterBody2D
 
 @export var speed: float = 300.0
 
+func _ready() -> void:
+	print("Window size: ", get_window().size)
+	print("Viewport size: ", get_viewport().size)
+
+func _on_resized() -> void:
+	print("Resized! New size: ", get_window().size)
+	
 func _physics_process(delta: float) -> void:
 	var input_vector := Vector2.ZERO
 	
---
+++ UI/Timer/timer.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var timer: Timer = $Timer
+@onready var label: Label = $Label
+
+signal timer_reached_zero
+
+@export var max_time:int = 15
+var remaining_time: int:
+	set(value):
+		remaining_time = value
+		if remaining_time <= 0:
+			_self_timer_finished()
+
+func _self_timer_finished() -> void:
+	print("GG")
+	timer_reached_zero.emit()
+
+func _on_timer_timeout() -> void:
+	remaining_time -= 1
+	updateValue()
+
+func _ready() -> void:
+	remaining_time = max_time
+	updateValue()
+
+func updateValue() -> void:
+	label.text = str(remaining_time)
