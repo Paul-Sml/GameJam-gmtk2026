8f17096aeac0580adbdf1849bc931ab13ac6bd76
Paul-Sml <paul.semmel.pro@gmail.com>
2026-07-24 19:24:56 +0200

    loop working except monsters are not moving

--- UI/Timer/timer.gd
+++ UI/Timer/timer.gd
@@ -1,21 +1,17 @@
 extends Control
+class_name GameTimer
 
 @onready var timer: Timer = %Timer
 @onready var label: Label = %Label
 
 signal timer_reached_zero
 
-@export var max_time: int = 15
 var remaining_time: int:
 	set(value):
 		remaining_time = value
 		if remaining_time == 0:
 			timer.stop()
 			timer_reached_zero.emit()
-
-func _ready() -> void:
-	remaining_time = max_time
-	updateValue()
 
 func start_timer(time: int) -> void:
 	remaining_time = time
--- game_manager.gd
+++ game_manager.gd
@@ -1,24 +1,53 @@
 extends Node2D
 
+var level_id: int = 0
+var levels: Array[String] = [
+	"res://Maps/level1.tscn",
+	"res://Maps/level0.tscn",
+	"res://Maps/level-1.tscn"
+]
+var timers: Array[int] = [
+	10,
+	15,
+	20
+]
+
+
 @onready var current_level: Node2D = %CurrentLevel
 @onready var canvas_layer: CanvasLayer = %CanvasLayer
 @onready var level_down_menu: LevelDownMenu = %LevelDownMenu
-
+@onready var timer: GameTimer = %Timer
 
 @onready var armor_icons: HBoxContainer = %ArmorIcons
 
 func _ready() -> void:
-	new_level()
+	new_stage()
 
-func new_level() -> void:
-	#load level
+func new_stage() -> void:
+	print("new level !")
+	load_stage(level_id)
+	level_id += 1
 	var player: Player = find_player_in_current_level()
 	if player == null:
 		push_error("No player found in current level")
 		return
-	else:
-		player.armor_updated.connect(_on_player_armor_updated)
-		print("Player found in current level: ", player.name)
+	player.armor_updated.connect(_on_player_armor_updated)
+
+	current_level.visible = true
+	current_level.process_mode = Node2D.PROCESS_MODE_INHERIT
+	canvas_layer.visible = true
+
+
+func load_stage(index: int) -> void:
+	print("loading level ", index)
+	for child in current_level.get_children():
+		child.queue_free()
+	
+	var level_scene: PackedScene = load(levels[index])
+	var level_instance: Node = level_scene.instantiate()
+	
+	current_level.add_child(level_instance)
+	timer.start_timer(timers[index])
 
 func find_player_in_current_level() -> Player:
 	for player in get_tree().get_nodes_in_group("player"):
@@ -37,3 +66,11 @@ func _on_timer_timer_reached_zero() -> void:
 	current_level.process_mode = Node2D.PROCESS_MODE_DISABLED
 	canvas_layer.visible = false
 	level_down_menu.level_down()
+
+
+func _on_next_level() -> void:
+	new_stage()
+
+
+func _on_level_down_menu_next_level() -> void:
+	pass # Replace with function body.
